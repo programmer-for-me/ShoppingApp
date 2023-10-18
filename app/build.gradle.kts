858d19fc3f73576aa8d859b660d66f848d0b674c
Msi <elshodxushvaqtov07@gmail.com>
2023-10-18 10:58:46 +0500

    WithAdapter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,5 +54,4 @@ dependencies {
     implementation("io.coil-kt:coil:2.4.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("me.relex:circleindicator:2.1.4")
 }
---
+++ app/src/main/java/com/example/shoppingapp/adapter/ViewPagerAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.shoppingapp.adapter
+
+class ViewPagerAdapter {
+}
