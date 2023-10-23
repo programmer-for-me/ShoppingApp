4b5c0de58ab583273fe20b25e1bde78d26462412
programmer-for-me <programmer.for.me@gmail.com>
2023-10-23 10:18:46 +0500

    tamom

--- app/src/main/java/com/example/shoppingapp/fragments/ProductsFragment.kt
+++ app/src/main/java/com/example/shoppingapp/fragments/ProductsFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -24,7 +25,6 @@ import retrofit2.Response
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -67,7 +67,8 @@ class ProductsFragment : Fragment() {
                 var products = response.body()!!.products
                 var productsAdapter = FilterAdapter(products, object : FilterAdapter.ProductInterface{
                     override fun productOnClick(id: Int) {
-                        param1 = id
+                        var bundle = bundleOf(Pair("id", id))
+                        findNavController().navigate(R.id.action_productsFragment_to_productInfoFragment, bundle)
                     }
 
                 })
