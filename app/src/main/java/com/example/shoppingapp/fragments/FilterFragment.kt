fca11970cd0e71fa91af33c12036081b726d7a2a
Msi <elshodxushvaqtov07@gmail.com>
2023-10-23 10:28:24 +0500

    WithAdapter

--- app/src/main/java/com/example/shoppingapp/fragments/FilterFragment.kt
+++ app/src/main/java/com/example/shoppingapp/fragments/FilterFragment.kt
@@ -98,7 +98,12 @@ class FilterFragment : Fragment() {
                             response: Response<ProductData>
                         ) {
                             list = response.body()?.products!!
-                            val mainAdapter = FilterAdapter(list)
+                            val mainAdapter = FilterAdapter(list,object : FilterAdapter.ProductInterface{
+                                override fun productOnClick(id: Int) {
+                                    var bundle = bundleOf("id" to id)
+                                    findNavController().navigate(R.id.action_mainFragment_to_productInfoFragment, bundle)
+                                }
+                            })
                             var hourManager = GridLayoutManager(context, 2)
                             binding.recycler.layoutManager = hourManager
                             binding.recycler.adapter = mainAdapter
--- app/src/main/java/com/example/shoppingapp/fragments/ProductInfoFragment.kt
+++ app/src/main/java/com/example/shoppingapp/fragments/ProductInfoFragment.kt
@@ -14,13 +14,16 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+private const val ARG_PARAM1 = "param1"
+
 class ProductInfoFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
+    private var param1: Int? = null
     private lateinit var binding: FragmentProductInfoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        arguments?.let {
+            param1 = it.getInt(ARG_PARAM1)
+        }
     }
 
     override fun onCreateView(
@@ -29,18 +32,20 @@ class ProductInfoFragment : Fragment() {
     ): View {
         binding = FragmentProductInfoBinding.inflate(layoutInflater)
         val api = APIClient.getInstance().create(APIService::class.java)
+        if (arguments?.containsKey("id") == true) {
+            api.getProduct(arguments?.getInt("id")!!)
+                .enqueue(object : Callback<Product> {
+                    override fun onResponse(call: Call<Product>, response: Response<Product>) {
+                        if (response.isSuccessful && response.body() != null)
+                            Log.d("AAA", "onResponse: ${response.body()?.title}")
+                    }
 
-        api.getProduct(1).enqueue(object : Callback<Product> {
-            override fun onResponse(call: Call<Product>, response: Response<Product>) {
-                if (response.isSuccessful && response.body() != null)
-                    Log.d("AAA", "onResponse: ${response.body()?.title}")
-            }
+                    override fun onFailure(call: Call<Product>, t: Throwable) {
+                        Log.d("AAB", "onFailure: $t")
+                    }
 
-            override fun onFailure(call: Call<Product>, t: Throwable) {
-                Log.d("AAB", "onFailure: $t")
-            }
-
-        })
+                })
+        }
         return binding.root
     }
 
